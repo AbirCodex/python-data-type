a9583e8fcc56fea83f8526bdb3b7daff627f24e6
AbirCodex <abirhassan66@gmail.com>
2025-07-19 10:34:50 +0600

    add a line

---
+++ python-data-type.py
@@ -0,0 +1,57 @@
+# #Create a list
+# frutes = ['
+# apple', 'banana', 'cherry', 'orange', 'kiwi', 'mango']
+# print(frutes)
+# print(f"Innitial frutes: {frutes}")
+# #Accessing elements in a list
+# print(f"First frute:{frutes[0]}")
+# print(f"Last frute:{frutes[-1]}")
+# modiqfying elements in a list
+# frutes[0] = 'avocado'
+# print(f"Modified frutes: {frutes}")
+# #Adding elements to a list
+# frutes.append('grape')
+# print(f"After adding frutes: {frutes}")
+# frutes.insert(2, 'pear')
+# print(f"After inserting frutes: {frutes}")
+# #Removing elements from a list
+# frutes.remove('banana')
+# print(f"After removing frutes: {frutes}")
+# #Removing the last element
+# last_frute = frutes.pop()
+# print(f"After popping last frute: {frutes}, popped frute: {last_frute}")
+# del frutes[1]
+# print(f"After deleting frute at index 1: {frutes}")
+
+# Sorting a list
+# frutes.sort()
+# print(f"Sorted frutes: {frutes}")
+# #Reversing a list
+# frutes.reverse()
+# print(f"Reversed frutes: {frutes}")
+
+
+#List slicing (getting a sub list )
+# numbers = [30, 10, 20, 50, 40]
+# # subsert = numbers[3:4]  # Get elements from index 1 to 3]
+# # print(f"Subset of numbers: {subsert}")
+# #List comprehension
+# squares_numbers = [x**2 for x in range(20)]
+# print(f"Squares of numbers: {squares_numbers}")
+# # Filtering even numbers using list comprehension
+# even_numbers = [x for x in range(20) if x % 2 == 0]
+# print(f"Even numbers: {even_numbers}")
+# # Filtering odd numbers using list comprehension
+# odd_numbers = [x for x in range(20) if x % 2 != 0]
+# print(f"Odd numbers: {odd_numbers}")
+#nested lists
+# nested_list = [
+#             [1, 2, 3], 
+#             [4, 5, 6], 
+#             [7, 8, 9]
+#                ]
+# print(f"Nested list: {nested_list}")
+# print(f"First element of first sublist: {nested_list[0][0]}")
+# print(f"Last element of last sublist: {nested_list[-1][-1]}")
+
+
